6209077926213006e24791d8ad6371c2d9a672bc
Nathan03nable <nathan.pringle@n-able.com>
2024-06-05 12:57:11 +0100

    Email

---
+++ pkg/email.go
@@ -0,0 +1,35 @@
+package pkg
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"net/smtp"
+	"os"
+)
+
+func SendEmail(subject string, body string) error {
+	_ = godotenv.Load("../../.env.development.local")
+
+	emailUsername := os.Getenv("EMAIL_USERNAME")
+
+	auth := smtp.PlainAuth(
+		"",
+		emailUsername,
+		os.Getenv("EMAIL_PASSWORD"),
+		"smtp.gmail.com",
+	)
+
+	to := []string{"doroyi1377@jzexport.com"}
+	msg := []byte("To: doroyi1377@jzexport.com\r\n" +
+		"Subject: " + subject +
+		"\r\n" +
+		body + "\r\n")
+
+	err := smtp.SendMail("smtp.gmail.com:587", auth, emailUsername, to, msg)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
